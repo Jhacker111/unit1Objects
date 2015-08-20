f001808c886883edab40a47c35f6e32ce8934c6e
Jhacker111 <Jhacker111@aol.com>
2015-08-20 10:30:53 -0500

    Did stuff
    did java

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -8,6 +8,27 @@ public class TurtleWorld
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.ORANGE);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.YELLOW);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.GREEN);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.BLUE);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.MAGENTA);
+        turtle.penDown();
+        turtle.turnRight(90);
         turtle.forward(50);
     }
 }
