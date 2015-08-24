1b00b6b1a6767219a3f896cbf1c45a46f822710d
Jhacker111 <Jhacker111@aol.com>
2015-08-24 10:27:58 -0500

    easter eggs
    jk we did a hello world

---
+++ ch1practice/TurtleWorld.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+
+public class TurtleWorld
+{
+    public static void main(String args[])
+    {
+        World turtleWorld = new World();
+        Turtle turtle = new Turtle(turtleWorld);
+        turtle.setPenColor(Color.RED);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.ORANGE);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.YELLOW);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.GREEN);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.BLUE);
+        turtle.penDown();
+        turtle.right(90);
+        turtle.forward(50);
+        turtle.setPenColor(Color.MAGENTA);
+        turtle.penDown();
+        turtle.turnRight(90);
+        turtle.forward(50);
+    }
+}
