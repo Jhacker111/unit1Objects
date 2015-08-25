92625a7e0a0253c445aa9214d2a0094501bc2959
Jhacker111 <Jhacker111@aol.com>
2015-08-25 10:08:41 -0500

    added some example problems
    Keep it :100:

---
+++ ch1practice/Tast.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class Tast
+{
+    public static void main(String[] args) throws Exception
+    {
+        URL imageLocation = new URL(
+            "http://media.giphy.com/media/ToMjGpGNs70xbxTuiBy/giphy.gif");
+        JOptionPane.showMessageDialog(null, "Hello", "Title",
+            JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
+        }
+    }
