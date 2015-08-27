d60d20f26eae259bf75de4d923d8c97ce1b8ab52
Jhacker111 <Jhacker111@aol.com>
2015-08-27 10:30:53 -0500

    unit 2
    added unit 2

---
+++ Chapter 2 Practice/String1.java
@@ -0,0 +1,10 @@
+public class String1
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Hello, world";
+        int n = greeting.length();
+        
+        System.out.println("Length of string: " + n);
+    }
+}
